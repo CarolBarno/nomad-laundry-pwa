497ed97718976256349d01b6897dc7872ff67803
Barno <32536886+CarolBarno@users.noreply.github.com>
2022-10-07 12:15:42 +0300

    current user navbar (#4)
    Co-authored-by: Carol-Barno <cbarno@innovexsolutions.co.ke>

--- src/app/service/data.service.ts
+++ src/app/service/data.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
+import { CurrentUser } from '../interface/user-interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
+  currentUser: CurrentUser | any;
   constructor() { }
 
   logError(error: Error) {
